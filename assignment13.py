fcb02a4e33011a520f86934cab0bc9494a05e8f1
AhmetEker1973 <demircioglua011@gmail.com>
2022-11-02 09:17:37 +0300

    # Solution to the Assignment-13 (Python - Is it a Prime Number?)

---
+++ assignment13.py
@@ -0,0 +1,27 @@
+# Assignment-13 (Python - Is it a Prime Number?)
+
+sayı = int(input("Please enter a number"))
+
+if sayı > 1 :
+
+    divisors = range(2, sayı)
+    
+    for i in divisors :
+        if sayı == 2 :
+            print(f"{sayı} is a prime number")
+            break
+        elif sayı % i == 0 :
+            print(f"{sayı} is not a prime number")
+            break 
+    else :
+        print(f"{sayı} is a prime number")
+  
+else :
+    print("Invalid entry. Please enter a positive number greater than 1!")
+
+Please enter a number: 9
+9 is not a prime number
+Please enter a number: 1
+Invalid entry. Please enter a positive number greater than 1!
+Please enter a number: 25
+25 is not a prime number
