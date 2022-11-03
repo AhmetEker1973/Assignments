7379604eb176cbf702a7749721979d43e445ce90
AhmetEker1973 <demircioglua011@gmail.com>
2022-11-04 02:44:44 +0300

    Assignment_14 added

---
+++ assignment14.py
@@ -0,0 +1,15 @@
+fibonacci = [1]  # adds the first “1” manually.
+
+liste = list(range(1, 2))
+
+while True  :
+    i = liste[len(liste)-2] + liste[len(liste)-1]
+    liste.append(i)
+        
+    if liste[len(liste) - 1] == 55 :
+        break
+
+fibonacci = fibonacci + liste
+print(f"fibonacci -> ", fibonacci)
+
+fibonacci ->  [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
